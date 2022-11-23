48296631416916ecc8f0e82b35082e219f7b5d18
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-23 22:53:48 +0800

    update

--- session-clone/src/pages/AddNew.js
+++ session-clone/src/pages/AddNew.js
@@ -1,11 +1,35 @@
 import React from "react";
+import axios from "axios";
 
 export default class AddNew extends React.Component {
+    base_url = "http://localhost:8000/";
 
-    render(){
-        return(
+    state = {
+        name: "",
+        countryId: "",
+        brandId: "",
+
+        countries: [],
+        brands: []
+    }
+
+    async componentDidMount() {
+        let brandsResponse = await axios.get(this.base_url + "brands");
+        // console.log(brandsResponse.data);
+        let countriesResponse = await axios.get(this.base_url + "countries");
+        // console.log(countriesResponse.data);
+
+        this.setState({
+            countries: countriesResponse.data,
+            brands: brandsResponse.data
+        })
+    }
+
+    render() {
+        return (
             <React.Fragment>
                 <h1>Add a new tool here</h1>
+                <input ></input>
             </React.Fragment>
         )
     }
