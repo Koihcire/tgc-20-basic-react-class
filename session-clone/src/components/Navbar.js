6415053b626247a696160cf588f495d7a9a7b510
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-23 22:40:23 +0800

    test

--- session-clone/src/components/Navbar.js
+++ session-clone/src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 export default class Navbar extends React.Component {
-    handleChange(active){
-        this.props.changePage(active);
-    }
 
     render() {
         return (
@@ -18,7 +15,7 @@ export default class Navbar extends React.Component {
                                 </div>
                             </li>
                             <li className="nav-item">
-                                <div className={"nav-link" + (this.props.activePage === "search" ? " active" : "")} aria-current="page" onClick={() => this.handleChange("search")}>
+                                <div className={"nav-link" + (this.props.activePage === "search" ? " active" : "")} aria-current="page" onClick={() => this.props.changePage("search")}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                     </svg> <span className="navTitle">Search</span>
