a03e6329df2dfeeab35f6aed795f6c31c3869db7
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-25 00:10:05 +0800

    update

--- session-clone/src/pages/Search.js
+++ session-clone/src/pages/Search.js
@@ -1,11 +1,38 @@
 import React from "react";
+import "../index.css"
 
 export default class Search extends React.Component {
 
-    render(){
-        return(
+
+    state = {
+        isDropboxOpen: false //CUSTOM DROPDOWN WITH TRANSITION
+    }
+
+    // CUSTOM DROPDOWN WITH TRANSITION
+    toggleDropbox() {
+        if (!this.state.isDropboxOpen) {
+            this.setState({
+                isDropboxOpen: true
+            })
+        }
+        if (this.state.isDropboxOpen){
+            this.setState({
+                isDropboxOpen: false
+            })
+        }
+    }
+
+    render() {
+        return (
             <React.Fragment>
                 <h1>This is the Search Page</h1>
+
+                {/* CUSTOM DROPDOWN WITH TRANSITION */}
+                <div className="container">
+                    <button className="btn btn-primary btn-sm" onClick={this.toggleDropbox.bind(this)}>Click</button>
+                    <div id="dropbox" className={this.state.isDropboxOpen ? "showDropbox" : "hideDropbox"}></div>
+                </div>
+                
             </React.Fragment>
         )
     }
