943f12da8c8f3d3b8717ef7cb963cee11dfa0427
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-24 08:52:36 +0800

    update

--- session-clone/src/pages/AddNew.js
+++ session-clone/src/pages/AddNew.js
@@ -10,7 +10,9 @@ export default class AddNew extends React.Component {
         brandId: "",
 
         countries: [],
-        brands: []
+        brands: [],
+        
+        newCountry: ""
     }
 
     async componentDidMount() {
@@ -25,11 +27,86 @@ export default class AddNew extends React.Component {
         })
     }
 
+    updateFormField = (e) => {
+        if (e.target.type === "checkbox") {
+            let currentValues = this.state[e.target.name];
+            let modifiedValues;
+            if (!currentValues.includes(e.target.value)) {
+                modifiedValues = [...currentValues, e.target.value];
+            } else {
+                modifiedValues = currentValues.filter((element) => {
+                    return element !== e.target.value
+                })
+            }
+            this.setState({
+                [e.target.name]: modifiedValues
+            })
+        } else {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    async submit(){
+        //initialise the attributes
+        let name = this.state.name;
+        let countryId = this.state.countryId;
+        let brandId = this.state.brandId;
+
+        //if other country is selected, then create the entry for the country first
+        if (countryId === "other"){
+            let response = await axios.post(this.base_url + "create-country" ,{
+                country: this.state.newCountry
+            })
+            console.log(response);
+            countryId = response.data.insertedId;
+        }
+
+        let response = await axios.post(this.base_url + "create-listing", {
+            name,
+            countryId,
+            brandId
+        })
+
+        console.log(response);
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h1>Add a new tool here</h1>
-                <input ></input>
+                
+                <h4>Listing Name</h4>
+                <input name="name" type="text" className="form-control" value={this.state.name} onChange={this.updateFormField}></input>
+                
+                <h4>Country</h4>
+                <select className="form-select" name="countryId" onChange={this.updateFormField}>
+                    <option selected>Select One</option>
+                    {this.state.countries.map(country => (
+                        <React.Fragment>
+                            <option value={country._id}>{country.country}</option>
+                        </React.Fragment>
+                    ))}
+                    <option value="other">Other</option>
+                </select>
+                {/* conditionally render an input box for user to key in country name, if they select other */}
+                {this.state.countryId === "other" ? 
+                    <div>
+                        <input name="newCountry" type="text" className="form-control" value={this.state.newCountry} onChange={this.updateFormField} placeholder="Add New Country"></input>
+                    </div> : ""
+                }
+
+                <h4>Brand</h4>
+                <select className="form-select" name="brandId" onChange={this.updateFormField}>
+                    <option selected>Select One</option>
+                    {this.state.brands.map(brand => (
+                        <React.Fragment>
+                            <option value={brand._id}>{brand.brandName}</option>
+                        </React.Fragment>
+                    ))}
+                </select>
+                <button className="btn btn-primary" onClick={this.submit.bind(this)}>Submit</button>
             </React.Fragment>
         )
     }
